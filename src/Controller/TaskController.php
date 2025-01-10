27bf1bb24a8abaef1d8657da69d8385f3394c3c5
Andrew Farquharson <farquharson.a@gmail.com>
2025-01-10 21:00:14 +0000

    add task controller & template

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TaskController extends AbstractController
+{
+    #[Route('/task', name: 'app_task')]
+    public function new(Request $request): Response
+    {
+        // creates a task object and initializes some data
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTimeImmutable('tomorrow'));
+
+        $form = $this->createFormBuilder($task)
+            ->add('task', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->add('save', SubmitType::class, ['label' => 'Create Task'])
+            ->getForm();
+
+        return $this->render('task/task.html.twig', [
+            'form' => $form,
+            'controller_name' => 'TaskController',
+        ]);
+    }
+}
