ed256a25fcac6eced19332804d181eba1fe4dd15
Andrew Farquharson <farquharson.a@gmail.com>
2025-01-13 00:01:29 +0000

    create UserFactory and fake users

---
+++ src/Factory/UserFactory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+
+/**
+ * @extends PersistentProxyObjectFactory<User>
+ */
+final class UserFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return User::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        $firstName = self::faker()->firstName();
+        $email = strtolower($firstName).'@domain.com';
+        return [
+            'email' => $email,
+            'roles' => [],
+            'firstName' => $firstName,
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(User $user): void {})
+        ;
+    }
+}
