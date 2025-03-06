e46ca4c6a41c767368a3c1d47bf0becc04e29cc5
Andrew Farquharson <farquharson.a@gmail.com>
2025-03-06 22:13:39 +0000

    update product controller

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    #[Route('/product', name: 'create_product')]
+    public function createProduct(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(1999);
+        $product->setDescription('Ergonomic and stylish!');
+
+        // indicate you want to eventually save Product but no queries yet
+        $entityManager->persist($product);
+
+        // actually executes the queries i.e. the INSERT query
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
     }
 }
