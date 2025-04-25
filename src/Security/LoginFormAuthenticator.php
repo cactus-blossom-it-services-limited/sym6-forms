510f2e2110802a44b6c51cfd8dd8d255503893f7
Andrew Farquharson <farquharson.a@gmail.com>
2025-04-25 08:27:01 +0100

    added csrf token back in form

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -13,6 +13,7 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
@@ -56,7 +57,8 @@ class LoginFormAuthenticator extends AbstractAuthenticator
                 new CsrfTokenBadge(
                     'authenticate',
                     $request->request->get('_csrf_token')
-                )
+                ),
+                new RememberMeBadge(),
             ]
         );
     }
